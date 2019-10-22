79d513413acd251e7bb5326afa15fab18f56449d
wonderfulnx <naxinlegend@outlook.com>
2019-10-23 01:19:22 +0800

    realize record process

--- asonicOFDM/app/src/main/java/com/example/asonicofdm/MainActivity.java
+++ asonicOFDM/app/src/main/java/com/example/asonicofdm/MainActivity.java
@@ -1,55 +1,94 @@
 package com.example.asonicofdm;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import com.example.asonicofdm.Recorder;
+
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    protected static final String TAG = "RecordActivity";
+    private Button start_btn;
+    private EditText editText;
+    private Recorder recorder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        getPermission();
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // ------------- UI init
+        this.editText = this.findViewById(R.id.editText);
+        this.editText.setKeyListener(null);
+
+        this.start_btn = this.findViewById(R.id.record);
+        this.start_btn.setOnClickListener(new View.OnClickListener() {
+            boolean recording = false;
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (this.recording) {
+                    this.recording = false;
+                    //结束录音
+
+                    recorder.recording = false;
+                    try {
+                        recorder.join();
+                    } catch (InterruptedException e){
+                        Log.e(TAG, "record thread Interrupted;");
+                    }
+
+                    MainActivity.this.logToDisplay("end the record.");
+                    MainActivity.this.start_btn.setText("Start Record");
+                }
+                else {
+                    this.recording = true;
+                    // 开始录音
+                    MainActivity.this.logToDisplay("start the record.");
+                    recorder = new Recorder();
+                    recorder.start();
+                    MainActivity.this.start_btn.setText("Stop Record");
+                }
             }
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void getPermission() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)!=
+                PackageManager.PERMISSION_GRANTED||
+        ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!=
+                PackageManager.PERMISSION_GRANTED||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!=
+                        PackageManager.PERMISSION_GRANTED
+        ) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.RECORD_AUDIO,
+                            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void logToDisplay(final String msg) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                MainActivity.this.editText.append(msg + "\n");
+            }
+        });
     }
 }
