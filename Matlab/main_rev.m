e2c9b5915337495cfbb7d49cb7c26702014746ee
wonderfulnx <naxinlegend@outlook.com>
2019-10-27 17:09:40 +0800

    added bin to str show

--- Matlab/main_rev.m
+++ Matlab/main_rev.m
@@ -28,11 +28,13 @@ grid on
 
 % % ========================== BER计算 ==========================
 bits = textread('data/data.txt');
+str = Bin2String(bits);
 error_bits = 0;
 for i = 1:length(bits)
     if bits(i) ~= Rx_bits(i)
         error_bits = error_bits + 1;
     end
 end
+fprintf('String: %s\n', char(str));
 fprintf('BER: %f\n', error_bits / length(bits));
 
