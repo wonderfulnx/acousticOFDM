977c9aefc54c762d0ffa5c4b6f1dca724e29a22d
NaXin <naxinlegend@outlook.com>
2020-11-01 14:24:48 +0800

    added some debug code

--- Matlab/main_rev.m
+++ Matlab/main_rev.m
@@ -31,7 +31,7 @@ fig_len = 4 * con.d;
 axis([-fig_len, fig_len, -fig_len, fig_len]);
 grid on
 
-% % ========================== BER计算 ==========================
+% % ========================== BER计算 =============================
 bits = textread('data/data.txt');
 fprintf('Origin: %s\n', char(Bin2String(bits)));
 error_bits = 0;
@@ -43,3 +43,19 @@ end
 
 fprintf('BER: %f\n', error_bits / length(bits));
 
+
+%% ========================== Debug Code ==========================
+% BERS = [];
+% len = length(Rx_sound);
+% data_len = con.symbol_per_carrier * (con.IFFT_length + con.GI) + con.GIP;
+% for sta = 1:len - data_len + 1
+%     [Rx_bits, Rx_complex_mat] = OFDM_dmod(con, Rx_sound(sta:sta + data_len - 1));
+%     error_bits = 0;
+%     for i = 1:length(bits)
+%         if bits(i) ~= Rx_bits(i)
+%             error_bits = error_bits + 1;
+%         end
+%     end
+%     BERS = [BERS, error_bits / length(bits)];
+% end
+% plot(BERS)
